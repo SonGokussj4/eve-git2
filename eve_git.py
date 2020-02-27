e5bfe15f256a4c8caa5f6c0cba681cbb9b08b9f8
Verner Jan <jverner@evektor.cz>
2020-02-27 16:25:31 +0100

    First working deploy, for now fixed parameters

--- eve_git.py
+++ eve_git.py
@@ -10,7 +10,11 @@
 import os
 import sys
 import json
+import shlex
+import shutil
+import filecmp
 import getpass
+import subprocess as sp
 from pathlib import Path
 # from dataclasses import dataclass
 import requests
@@ -46,6 +50,7 @@ SCRIPTDIR = Path(__file__).resolve().parent
 CURDIR = Path('.')
 SERVER = cfg.Server.url
 GITEA_TOKEN = cfg.Server.gitea_token
+SKRIPTY_DIR = Path("/expSW/SOFTWARE/skripty")
 
 
 # ===============================
@@ -59,6 +64,136 @@ from progress import Progress
 # =================================
 def deploy(args):
     print(f"[ INFO ] Deploying... args: '{args}'")
+
+    # User specified both arguments: --clone <reponame> <username>
+    if len(args) >= 2:
+        branch = 'master'
+        if len(args) == 3:
+            reponame, username, branch = args
+        else:
+            reponame, username = args
+
+        # Does the username exist?
+        res = requests.get(f"{SERVER}/api/v1/users/{username}")
+        if res.status_code != 200:
+            print(f"[ ERROR ] User '{username}' doesn't exist!")
+            sys.exit(1)
+
+        # Does the <repository> of <user> exist?
+        res = requests.get(f"{SERVER}/api/v1/repos/{username}/{reponame}")
+        if res.status_code != 200:
+            print(f"[ ERROR ] Repository '{SERVER}/{username}/{reponame}' does not exist.")
+            sys.exit(1)
+
+        # Everything OK, clone the repository to /tmp/<reponame>
+        tmp_dir = Path('/tmp') / reponame
+        print("DEBUG: tmp_dir:", tmp_dir)
+
+        # If folder exists, delete
+        if tmp_dir.exists():
+            print(f"[ WARNING ] '{tmp_dir}' already exists. Removing.")
+            # res = shutil.rmtree(tmp_dir, ignore_errors=True)
+            res = shutil.rmtree(tmp_dir)
+
+        # Clone repo
+        repo = Repo.clone_from(url=f"{SERVER}/{username}/{reponame}",
+                                   to_path=tmp_dir.resolve(),
+                                   progress=Progress())
+        print(f"[ INFO ] Clonning to '{tmp_dir}' DONE")
+        print(f"[ DEBUG ] repo: {repo}")
+
+        # Remove .git folder
+        print(f"[ DEBUG ] Removing '.git' folder")
+        res = shutil.rmtree(tmp_dir / '.git')
+        print(f"[ DEBUG ] Removing '.git' folder ... DONE")
+
+        # Check if <reponame> already exists in /expSW/SOFTWARE/skripty/<reponame>
+        target_dir = SKRIPTY_DIR / reponame
+        print("DEBUG: target_dir:", target_dir)
+
+        # Create target_dir if it does not exists
+        if not target_dir.exists():
+            target_dir.mkdir(exist_ok=True)
+            print(f"[ DEBUG ] {target_dir} created.")
+
+        # Check for different in 'requirements.txt' file(s)
+        src_requirements = tmp_dir / 'requirements.txt'
+        if src_requirements.exists():
+            dst_requirements = target_dir / 'requirements.txt'
+            similar = filecmp.cmp(src_requirements, dst_requirements)
+            if not similar:
+                print("[ WARNING ] BEWARE. requirements.txt are different. Please 'pip install -r requirements.txt' changes.")
+
+        # TODO check req, mount/create .env, ... all this stuff
+
+        # Ask if they are SURE
+
+        # Rsync all the things
+        cmd = 'rsync -avh --progress --remove-source-files /tmp/dochazka/ ar-sword:/expSW/SOFTWARE/skripty/dochazka'
+        print("DEBUG: shlex(cmd):", shlex.split(cmd))
+        res = sp.call(shlex.split(cmd))
+        if res != 0:
+            print("[ ERROR ] Something went wrong with rsync...")
+            return 1
+
+        # Cleanup
+        shutil.rmtree(tmp_dir, ignore_errors=True)
+
+        # Check the description for 'what to do with .executable files and so on...'
+        print(f'[ INFO ] DONE')
+        return 0
+
+    # User didn't specify <username>: --clone <reponame>
+    elif len(args) == 1:
+        reponame = args[0]
+        res = requests.get(f"{SERVER}/api/v1/repos/search?q={reponame}&sort=created&order=desc")
+        print(f"[ DEBUG ] res: {res}")
+
+        data = json.loads(res.content)
+
+        # Check if there was a good response
+        if not data.get('ok'):
+            print(f"[ ERROR ] Shit... Data not acquired... {data}")
+            return 1
+        elif not data.get('data'):
+            print(f"[ ERROR ] Search for repository '{reponame}' returned 0 results... Try something different.")
+            return 1
+
+        # Data acquired, list all found repos in nice table
+        headers = ('id', 'repository', 'user', 'description')
+        results = [[item['id'], item['name'], item['owner']['login'], item['description']]
+                   for item in data.get('data')]
+        tbl = columnar(results, headers, no_borders=True)
+        print(tbl)
+
+        # Ask for repo ID
+        answer = input("Enter repo ID: ")
+        if not answer:
+            print("[ ERROR ] You have to write an ID")
+            return 1
+        elif not answer.isdigit():
+            print("[ ERROR ] What you entered is not a number... You have to write one of the IDs.")
+            return 1
+
+        # Get the right repo by it's ID
+        repo_id = int(answer)
+        selected_repository = [ls for ls in results if ls[0] == repo_id]
+
+        # User made a mistake and entered number is not one of the listed repo IDs
+        if len(selected_repository) == 0:
+            print(f"[ ERROR ] Not a valid answer. You have to select one of the IDs.")
+            sys.exit(1)
+
+        # Something went wrong. There should not be len > 1... Where's the mistake in the code?
+        elif len(selected_repository) > 1:
+            print(f"[ ERROR ] Beware! len(selected_repository) > 1... That's weird... "
+                  f"Like really... Len is: {len(selected_repository)}")
+            sys.exit(1)
+
+        print(f"[ INFO ] Deploying ID: {repo_id}")
+        reponame, username = selected_repository[0][1], selected_repository[0][2]
+        deploy([reponame, username])
+        return 0
 
     # Args nejspis "reponame" "username"
 
@@ -225,26 +360,27 @@ def create_repo(args_create):
         sys.exit(0)
 
 
-# TODO: trnasfer chybel v lokalni gitea (https://gitea.avalon.konstru.evektor.cz/api/swagger)
-# TODO: bude ve verzi 1.12.0
-# def transfer_repo():
-#     """To tranfer repo to some organization """
+def transfer_repo():
+    """To tranfer repo to some organization"""
+    # TODO: bude ve verzi 1.12.0
+    # TODO: trnasfer chybel v lokalni gitea (https://gitea.avalon.konstru.evektor.cz/api/swagger)
 
-#     reponame = "Test"
-#     description = "Test3"
-#     private = False
-#     organization = "P135"
-#     print("Server: ", SERVER)
-#     print("TOKEN: ", GITEA_TOKEN)
+    reponame = "Test"
+    description = "Test3"
+    private = False
+    organization = "P135"
+    print("Server: ", SERVER)
+    print("TOKEN: ", GITEA_TOKEN)
 
-#     repo_data = {'new_owner': organization}
-#     # repo_headers = {'accept': 'application/json', 'content-type': 'application/json',
-#     #                 'Authorization': 'token ACCESS_TOKEN'}
-#     res = requests.post(
-#         f"{SERVER}/api/v1/repos/ptinka/{reponame}/transfer?access_token={GITEA_TOKEN}",
-#         headers=repo_headers, json=repo_data)
-#     print(res)
-#     print(f"{SERVER}/api/v1/repos/ptinka/{reponame}/transfer?access_token={GITEA_TOKEN}")
+    repo_data = {'new_owner': organization}
+    # repo_headers = {'accept': 'application/json', 'content-type': 'application/json',
+    #                 'Authorization': 'token ACCESS_TOKEN'}
+    res = requests.post(
+        f"{SERVER}/api/v1/repos/ptinka/{reponame}/transfer?access_token={GITEA_TOKEN}",
+        headers=repo_headers, json=repo_data)
+    print(res)
+    print(f"{SERVER}/api/v1/repos/ptinka/{reponame}/transfer?access_token={GITEA_TOKEN}")
+    pass
 
 
 def list_org():
