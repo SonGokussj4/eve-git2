6b77270afe11cfa7473aa101833d6cdf515a4934
Verner Jan <jverner@evektor.cz>
2020-02-27 08:20:22 +0100

    Class Progress --> individual file; few cleanups

--- cli.py
+++ cli.py
@@ -19,7 +19,34 @@ class CustomHelpFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawTe
 
     def __init__(self, prog):
         super(CustomHelpFormatter, self).__init__(
-            prog, max_help_position=80, width=80)
+            prog, max_help_position=80, width=120)
+
+    # Remove unnecessary '<Commands>' line in '-h'
+    # Source: https://stackoverflow.com/a/48051233/4574809
+    def _format_action(self, action):
+            result = super()._format_action(action)
+            if isinstance(action, argparse._SubParsersAction):
+                # fix indentation on first line
+                return f"{self._current_indent * ' '}{result.lstrip()}"
+            return result
+
+    def _format_action_invocation(self, action):
+        if isinstance(action, argparse._SubParsersAction):
+            # remove metavar and help line
+            return ""
+        return super()._format_action_invocation(action)
+
+    def _iter_indented_subactions(self, action):
+        if isinstance(action, argparse._SubParsersAction):
+            try:
+                get_subactions = action._get_subactions
+            except AttributeError:
+                pass
+            else:
+                # remove indentation
+                yield from get_subactions()
+        else:
+            yield from super()._iter_indented_subactions(action)
 
 
 # =================================
@@ -28,7 +55,7 @@ class CustomHelpFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawTe
 def get_parser():
     """Return parser with arguments."""
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(title='commands', dest='command')
+    subparsers = parser.add_subparsers(title='commands', dest='command', metavar="<command>")
     parser.formatter_class = CustomHelpFormatter
 
     parser.version = __version__
@@ -37,6 +64,17 @@ Description:
    <Ideally one line description of the program>
 
 """
+    # https://pymotw.com/2/argparse/#nesting-parsers
+    parser_create = subparsers.add_parser('create', help='Subparser Help: create')
+    parser_create.add_argument('repository', help='Help for <repository>')
+    parser_create.add_argument('description', help='Help for <description>')
+
+    parser_deploy = subparsers.add_parser('sdeploy', help='Subparser Help: sdeploy')
+    parser_deploy.add_argument('repository', help='Help for <repository>')
+    parser_deploy.add_argument('username', help='Help for <username>')
+    parser_deploy.add_argument('branch', help='Help for <branch>')
+
+    # SUBPARSERS
     parser.add_argument('-v', action='count', help="Verbal")
 
     parser.add_argument('-V', '--version', action='version')
@@ -54,12 +92,6 @@ Description:
     group.add_argument('--list_org', dest='list_org',
                        action="store_true",
                        help='Show all organizations')
-
-    parser_deploy = subparsers.add_parser('sdeploy')
-    parser_deploy.add_argument('repository')
-    parser_deploy.add_argument('username')
-    parser_deploy.add_argument('branch')
-    parser_create = subparsers.add_parser('create')
 
     group.add_argument('--deploy', dest='deploy', nargs='+', type=str,
                        action=required_length(1, 3),
