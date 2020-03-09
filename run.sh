b4c376463c8fe0307680b2306664db98cfc00c05
Verner Jan <jverner@evektor.cz>
2020-03-09 16:46:56 +0100

    Flexible 'run.sh' now reads from 'repo.config' 3 variables before running program

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-ENV_NAME=".env"
-PRG_NAME=MAIN_FILE_PLACEHOLDER
-CUSTOM_LD_LIBRARY=/expSW/SOFTWARE/var/LD_LIBRARIES
-
 ## FIND DIRECTORY OF THE SCRIPT
 SOURCE="${BASH_SOURCE[0]}"
 
@@ -15,8 +11,15 @@ while [ -h "$SOURCE" ]; do
   # to the path where the symlink file was located
   [[ $SOURCE != /* ]] && SOURCE="$SCRIPTDIR/$SOURCE"
 done
+
 # Real script directory found
 SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+
+# LOAD VARIABLES FROM REPO.CONFIG
+source "${SCRIPTDIR}/repo.config" 2>/dev/null
+ENV_NAME="${name}"
+PRG_NAME="${main_file}"
+CUSTOM_LD_LIBRARY="${ld_lib}"
 
 ## ACTIVATE VIRTUAL ENVIRONMENT AND RUN APP
 source "$SCRIPTDIR"/${ENV_NAME}/bin/activate
