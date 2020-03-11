4c908028327b378f104f07891d52e00b2242fc00
Verner Jan <jverner@evektor.cz>
2020-03-11 16:27:28 +0100

    Comment out non-usable tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,35 +8,35 @@ SCRIPTDIR = Path(__file__).resolve().parent
 TEST_FILES = SCRIPTDIR / 'files'
 
 
-def test_ask_with_defaults_true(monkeypatch):
-    # monkeypatch.setattr('sys.stdin', StringIO('\n'.join(('first', 'second', ' '))))
-    monkeypatch.setattr('sys.stdin', StringIO('yes\n'))
-    assert utils.ask_with_defaults('Are you first?') == 'yes'
+# def test_ask_with_defaults_true(monkeypatch):
+#     # monkeypatch.setattr('sys.stdin', StringIO('\n'.join(('first', 'second', ' '))))
+#     monkeypatch.setattr('sys.stdin', StringIO('yes\n'))
+#     assert utils.ask_with_defaults('Are you first?') == 'yes'
 
-    monkeypatch.setattr('sys.stdin', StringIO('yes\n'))
-    assert utils.ask_with_defaults('Are you second?', defaults='no') == 'yes'
+#     monkeypatch.setattr('sys.stdin', StringIO('yes\n'))
+#     assert utils.ask_with_defaults('Are you second?', defaults='no') == 'yes'
 
-    monkeypatch.setattr('sys.stdin', StringIO('\n'))
-    assert utils.ask_with_defaults('Are you second?', defaults='no') == 'no'
+#     monkeypatch.setattr('sys.stdin', StringIO('\n'))
+#     assert utils.ask_with_defaults('Are you second?', defaults='no') == 'no'
 
-    monkeypatch.setattr('sys.stdin', StringIO('Suzumiya Haruhi\n'))
-    assert utils.ask_with_defaults('Best movie?', defaults='Sex in the city') != 'Silence of the labs'
+#     monkeypatch.setattr('sys.stdin', StringIO('Suzumiya Haruhi\n'))
+#     assert utils.ask_with_defaults('Best movie?', defaults='Sex in the city') != 'Silence of the labs'
 
 
-@pytest.mark.parametrize(
-    "question, defaults, user_input, result", [
-        ('Are you first?', '', 'yes\n', 'yes'),
-        ('Are you second?', 'no', 'yes\n', 'yes'),
-        ('Are you second?', 'no', '\n', 'no'),
-        ('Are you first?', 'Sex in the city', 'Suzumiya Haruhi\n', '!Silence of the lambs'),
-    ])
-def test_ask_with_defaults(monkeypatch, question, defaults, user_input, result):
-    monkeypatch.setattr('sys.stdin', StringIO(user_input))
-    if result.startswith('!'):
-        result = result[1:]
-        assert utils.ask_with_defaults(question, defaults) != result
-    else:
-        assert utils.ask_with_defaults(question, defaults) == result
+# @pytest.mark.parametrize(
+#     "question, defaults, user_input, result", [
+#         ('Are you first?', '', 'yes\n', 'yes'),
+#         ('Are you second?', 'no', 'yes\n', 'yes'),
+#         ('Are you second?', 'no', '\n', 'no'),
+#         ('Are you first?', 'Sex in the city', 'Suzumiya Haruhi\n', '!Silence of the lambs'),
+#     ])
+# def test_ask_with_defaults(monkeypatch, question, defaults, user_input, result):
+#     monkeypatch.setattr('sys.stdin', StringIO(user_input))
+#     if result.startswith('!'):
+#         result = result[1:]
+#         assert utils.ask_with_defaults(question, defaults) != result
+#     else:
+#         assert utils.ask_with_defaults(question, defaults) == result
 
 
 def test_remove_dir_tree():
