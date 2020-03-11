1841807622255d43d0d291bd05aafce406b465c4
Verner Jan <jverner@evektor.cz>
2020-03-11 16:42:31 +0100

    Notes in tests/__init__.py file

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+"""Docu model"""
+# Very good sources to read:
+# https://www.linuxjournal.com/content/testing-your-code-pythons-pytest
+# https://www.linuxjournal.com/content/testing-your-code-pythons-pytest-part-ii
+# https://www.linuxjournal.com/content/python-testing-pytest-fixtures-and-coverage
