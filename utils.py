5127c0d26864d1551cdecda7c7d6f5caf5250c29
Verner Jan <jverner@evektor.cz>
2020-03-16 07:49:43 +0100

    Remove @Autolog/@Traced from funcions in utils module

--- utils.py
+++ utils.py
@@ -104,8 +104,6 @@ def download(url: str, filepath):
     return filepath
 
 
-# @traced
-# @logged
 # def ask_with_defaults(question: str, defaults=''):
 #     """Return user input with optional default argument.
 
@@ -119,8 +117,6 @@ def download(url: str, filepath):
 #     return result
 
 
-@traced
-@logged
 def remove_dir_tree(dirpath):
     """Remove selected directory. Linux/Windows compatible."""
     if dirpath == 'str':
@@ -132,8 +128,6 @@ def remove_dir_tree(dirpath):
     return True
 
 
-@traced
-@logged
 def lineno(msg: str=None):
     if not msg:
         return sys._getframe().f_back.f_lineno
@@ -143,8 +137,6 @@ def lineno(msg: str=None):
           f"{msg if msg is not None else ''}")
 
 
-@traced
-@logged
 def requirements_similar(src_requirements: any, dst_requirements: any) -> bool:
     """Return True if both src_req and dst_req have the same content.
     Arguments:
@@ -166,8 +158,6 @@ def requirements_similar(src_requirements: any, dst_requirements: any) -> bool:
     return filecmp.cmp(src_requirements, dst_requirements)
 
 
-@traced
-@logged
 def lsremote(url):
     """Get references ('HEAD', 'refs/heads/<branch>') from remote <url> repo."""
     remote_refs = {}
@@ -178,8 +168,6 @@ def lsremote(url):
     return remote_refs
 
 
-@traced
-@logged
 def list_remote_branches(url: str) -> list:
     """Return list of branches from remote repository."""
     # GITEA API: /repos/{owner}/{repo}/branches
@@ -192,8 +180,6 @@ def list_remote_branches(url: str) -> list:
     return branches
 
 
-@traced
-@logged
 def remote_repo_branch_exist(url: str, branch: str) -> bool:
     """Return True if <branch> of remote <url> repository exists."""
     refs = lsremote(url)
@@ -203,8 +189,6 @@ def remote_repo_branch_exist(url: str, branch: str) -> bool:
     return False
 
 
-@traced
-@logged
 def check_user_repo_exist(server: str, repository: str, username: str, session) -> bool:
     """Return True if both 'user' and combination gitea 'user/repo' exists.
 
@@ -230,8 +214,6 @@ def check_user_repo_exist(server: str, repository: str, username: str, session) 
     return True
 
 
-@traced
-@logged
 def check_org_exist(server: str, organization: str, session) -> bool:
     """Return True if gitea 'organization' exists.
 
@@ -265,8 +247,6 @@ def check_org_exist(server: str, organization: str, session) -> bool:
     return True
 
 
-@traced
-@logged
 def make_symbolic_link(src_filepath: str, dst_filepath: str, remote_server: str = ''):
     if type(src_filepath) == 'str':
         src_filepath = Path(src_filepath)
@@ -284,8 +264,6 @@ def make_symbolic_link(src_filepath: str, dst_filepath: str, remote_server: str 
     return True
 
 
-@traced
-@logged
 def is_git_repo(path: any) -> bool:
     """Return True if 'path' is git repository, False otherwise."""
     try:
@@ -354,8 +332,6 @@ def select_files_from_list(directory: Path, hmmmm: bool = None, question: str = 
     return selected
 
 
-@traced
-@logged
 def get_repo_list_as_table(session: requests.Session(), server: str,
                            repository: str, username: str='') -> columnar:
     """Return columnar() table object with list of repositories using gitea api.
@@ -420,8 +396,6 @@ def get_repo_list_as_table(session: requests.Session(), server: str,
     return columnar(results, tbl_headers, no_borders=True, wrap_max=0)
 
 
-@traced
-@logged
 def select_repo_from_list(session: str, server: str, repository: str,
                           username: str = '', question: str = '') -> Selected:
     """Make columnar() table selectable, return Selected().
@@ -484,8 +458,6 @@ def select_repo_from_list(session: str, server: str, repository: str,
     return Selected(*answers)
 
 
-@traced
-@logged
 def get_org_list_as_table(session, server):
     """Return columnar() table object with list of organizations using gitea api.
 
@@ -541,8 +513,6 @@ def get_org_list_as_table(session, server):
     return columnar(results, headers, no_borders=True)
 
 
-@traced
-@logged
 def select_org_from_list(session, server, question):
     """Make columnar() table selectable, return Selected('organization') object.
 
@@ -594,8 +564,6 @@ def select_org_from_list(session, server, question):
     return selected
 
 
-@traced
-@logged
 def ask_confirm(msg):
     questions = [
         {
@@ -617,8 +585,6 @@ def ask_confirm(msg):
     return True
 
 
-@traced
-@logged
 def ask_confirm_data(msg, comp_str):
     questions = [
         {
@@ -646,8 +612,6 @@ def ask_confirm_data(msg, comp_str):
     return True
 
 
-@traced
-@logged
 def ask_confirm_from_list(msg, comp_str):
     questions = [
         {
