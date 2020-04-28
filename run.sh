c827e8907c645d0a7bfa4715115e2d266f66d0c5
SonGokussj4 <SonGokussj4@centrum.cz>
2020-04-28 10:00:18 +0200

    Few config changes

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# configFileName='${configFileName}'
+configFileName='app.conf'
+
 ## FIND DIRECTORY OF THE SCRIPT
 SOURCE="${BASH_SOURCE[0]}"
 
@@ -15,16 +18,16 @@ done
 # Real script directory found
 SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
 
-# Load variables from app.conf
-VENV_NAME=$(grep venv_name ${SCRIPTDIR}/app.conf | awk -F '[:=]' '{print $2}' | tr -d " ")
-MAIN_FILE=$(grep main_file ${SCRIPTDIR}/app.conf | awk -F '[:=]' '{print $2}' | tr -d " ")
+# Load variables from ${configFileName}
+VENV_NAME=$(grep venv_name ${SCRIPTDIR}/${configFileName} | awk -F '[:=]' '{print $2}' | tr -d " ")
+MAIN_FILE=$(grep main_file ${SCRIPTDIR}/${configFileName} | awk -F '[:=]' '{print $2}' | tr -d " ")
 if [[ $VENV_NAME == "" || $MAIN_FILE == "" ]]; then
-    echo "[ ERROR ] Can't load either venv_name: '$VENV_NAME' or main_file: '$MAIN_FILE' from '${SCRIPTDIR}/app.conf'..."
+    echo "[ ERROR ] Can't load either venv_name: '$VENV_NAME' or main_file: '$MAIN_FILE' from '${SCRIPTDIR}/${configFileName}'..."
     exit 1
 fi
 
-# Add custom LD_LIBRARY_PATH if set in app.conf
-CUSTOM_LD_LIBRARY=$(grep ld_lib ${SCRIPTDIR}/app.conf | awk -F '[:=]' '{print $2}' | tr -d " ")
+# Add custom LD_LIBRARY_PATH if set in ${configFileName}
+CUSTOM_LD_LIBRARY=$(grep ld_lib ${SCRIPTDIR}/${configFileName} | awk -F '[:=]' '{print $2}' | tr -d " ")
 if [[ CUSTOM_LD_LIBRARY != "" ]]; then
     # echo "[ DEBUG ] Adding '${CUSTOM_LD_LIBRARY}' to \$LD_LIBRARY_PATH"
     export LD_LIBRARY_PATH=${CUSTOM_LD_LIBRARY}:$LD_LIBRARY_PATH
--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,30 +1,33 @@
 # import os
 import utils
 import requests
+import configparser
 from pathlib import Path
+from eve_git import cfg
 
 
 def test_url_true():
-    url = 'http://gitea.avalon.konstru.evektor.cz/jverner/dochazka2/raw/branch/next/main.py'
+    url = f"{cfg['server']['url']}/C2/ansarun/raw/branch/master/ansarun.sh"
     res = requests.get(url)
     assert res.ok is True
 
 
 def test_url_false():
-    url = 'http://non-existing.avalon.konstru.evektor.cz'
+    url = f"{cfg['server']['url']}/C2/lalalulu"
     res = requests.get(url)
     assert res.ok is False
 
 
 def test_download_filepath_as_dir():
-    url = 'http://gitea.avalon.konstru.evektor.cz/jverner/dochazka2/raw/branch/next/main.py'
+    url = f"{cfg['server']['url']}/C2/ansarun/raw/branch/master/ansarun.sh"
     filename: Path = utils.download(url, Path('/tmp'))
+    print(f">>> filename: {filename}")
     filename.unlink()
-    assert filename == Path('/tmp/main.py')
+    assert filename == Path('/tmp/ansarun.sh')
 
 
 def test_download_filepath_as_filename():
-    url = 'http://gitea.avalon.konstru.evektor.cz/jverner/dochazka2/raw/branch/next/main.py'
+    url = f"{cfg['server']['url']}/C2/ansarun/raw/branch/master/ansarun.sh"
     filename: Path = utils.download(url, Path('/tmp/result.py'))
     filename.unlink()
     assert filename == Path('/tmp/result.py')
